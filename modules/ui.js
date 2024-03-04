e752e3271ccaebf42014f657c72a363bd4d16ada
Abdukhafizov Jamshed <furkatyusupov@MacBook-Air-Furkat.local>
2024-03-04 15:21:04 +0500

    add_dz

--- modules/ui.js
+++ modules/ui.js
@@ -1,57 +1,64 @@
+
+
 function getRGB() {
     function randomize() {
         return Math.floor(Math.random() * 255)
     }
-
+    
     let r = randomize()
     let g = randomize()
     let b = randomize()
-
+    
     return `rgb(${r}, ${g}, ${b})`
 }
 export function reload(arr, place) {
     place.innerHTML = ''
-
+    
     for (const item of arr) {
         let div_1 = document.createElement('div')
         let h3 = document.createElement('h3')
         let p = document.createElement('p')
-
+        let h4 = document.createElement("p")
+        
+        
         div_1.classList.add('card_visa')
+        h4.classList.add("about_h4")
         div_1.style.background = `linear-gradient(60deg, ${getRGB()}, ${getRGB()}, ${getRGB()})`
         h3.innerHTML = item.name
         p.innerHTML = item.currency
-
-        div_1.append(h3, p)
+        h4.innerHTML = `Ваш баланс: ${item.balance}` 
+        
+        div_1.append(h3, p, h4)
         place.append(div_1)
     }
 }
 
 export function reloadTransactions(arr, place, size) {
     place.innerHTML = ''
-
+    
     let tempArr = []
     if (size === 'small') tempArr = arr.slice(0, 7)
     else if (size === 'full') tempArr = arr
 
-    for(let item of tempArr) {
-        let tr = document.createElement('tr')
-        let idView = document.createElement('td')
-        let walletView = document.createElement('td')
-        let categoryView = document.createElement('td')
-        let sumView = document.createElement('td')
-        let daysAgoView = document.createElement('td')
-
-        place.append(tr)
-        tr.append(idView, walletView, categoryView, sumView, daysAgoView)
-
-        idView.innerHTML = item.id
-        walletView.innerHTML = item.wallet
-        categoryView.innerHTML = item.category
-        sumView.innerHTML = item.sum
-        daysAgoView.innerHTML = item.daysAgo + ' дней назад'
-    }
+for(let item of tempArr) {
+    let tr = document.createElement('tr')
+    let idView = document.createElement('td')
+    let walletView = document.createElement('td')
+    let categoryView = document.createElement('td')
+    let sumView = document.createElement('td')
+    let daysAgoView = document.createElement('td')
+    
+    place.append(tr)
+    tr.append(idView, walletView, categoryView, sumView, daysAgoView)
+    
+    idView.innerHTML = item.id
+    walletView.innerHTML = item.wallet
+    categoryView.innerHTML = item.category
+    sumView.innerHTML = item.sum
+    daysAgoView.innerHTML = item.daysAgo + ' дней назад'
 }
+}
+// import { getData } from "../../modules/http";
 
 export function createHeader() {
     let header = document.createElement('header')
@@ -63,7 +70,7 @@ export function createHeader() {
     let akcii = document.createElement('a')
     let email = document.createElement('a')
     let icon = document.createElement('img')
-
+    
     email.classList.add("user_mail")
     box.classList.add('box')
     box_l.classList.add('box_l')
@@ -73,30 +80,26 @@ export function createHeader() {
     koshelek.classList.add('koshelek')
     akcii.classList.add('akcii')
     icon.classList.add('icon')
-
+    
     document.body.prepend(header)
     header.append(box)
     box.append(box_l, box_r)
     box_l.append(glavnaya, koshelek, akcii)
     box_r.append(email, icon)
-
+    
     glavnaya.href = "/"
     koshelek.href = "/pages/wallets/"
     akcii.href = "/pages/transactions/"
-
-
+    
+    
     glavnaya.innerHTML = 'Главная'
     koshelek.innerHTML = 'Мои кошельки'
     akcii.innerHTML = "Мои транзакции"
     email.innerHTML = 'alexadams@google.com'
     icon.src = "../../public/log-out (1) 1.png"
-
+    
     icon.onclick = () => {
-        let btn = document.createElement('button')
-        btn.classList.add('btn')
-        btn.innerHTML = 'Выйти'
-        box_r.appendChild(btn);
-
+         location.assign('/pages/signin/')
     }
 
     return header
